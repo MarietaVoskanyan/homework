8f854e98045e6176e73f6717168314b31258f90a
MarietaVoskanyan <107118791+MarietaVoskanyan@users.noreply.github.com>
2022-06-26 02:12:52 +0400

    Update 1.3.py

--- 1.3.py
+++ 1.3.py
@@ -1,9 +1,11 @@
-def sum_of_max(a, b, c):
-    if a>b>c or b>a>c:
+def sum_of_max_squares(a, b, c):
+    
+    """ sum_of_max finds out the biggest two numbers and prints the sum of their squares """
+    if c < b < a or c < a < b:
         return (a**2 + b**2)
-    elif a>c>b or c>a>b:
+    elif b < c < a or b < a < c:
         return (a**2 + c**2)
-    elif b>c>a or c>b>a:
+    elif a < c < b or a < b < c:
         return (c**2 + b**2)
         
-print(sum_of_max(4, 2, 3))
+print(sum_of_max_squares(4, 2, 3))
