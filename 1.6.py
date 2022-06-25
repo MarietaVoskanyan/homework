abfd45f39cbfaf6bd9a8b3af679315d226f219e7
MarietaVoskanyan <107118791+MarietaVoskanyan@users.noreply.github.com>
2022-06-26 00:36:50 +0400

    Create 1.6.py

---
+++ 1.6.py
@@ -0,0 +1,31 @@
+def summ(a:float, b:float):
+    c = 0
+    d = 0
+    if a < b:
+        while a > c:
+           c += 1 
+        m = c 
+        while b > d:
+            d += 1
+        n = d - 1 
+        k = 0
+        sum = 0
+        while (k + m) <= n:
+            sum += (m + k)
+            k += 1 
+        return sum
+    if a > b:
+        while a > c:
+           c += 1 
+        m = c - 1
+        while b > d:
+            d += 1
+        n = d  
+        k = 0
+        sum = 0
+        while (k + n) <= m:
+            sum += (n + k)
+            k += 1 
+        return sum
+
+print(summ(7.75, 3.75))
