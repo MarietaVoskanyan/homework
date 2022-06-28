9b9d8f4983a5655a3cb309e9b13caaed367a0799
MarietaVoskanyan <107118791+MarietaVoskanyan@users.noreply.github.com>
2022-06-29 01:15:04 +0400

    Update split.py

--- split.py
+++ split.py
@@ -1,23 +1,15 @@
-def split(source, sep, count):
-	""" Split returns the list consisting of the subjects of source """
-	lst = []
-	i = 0
-	j = 0
-	while j <= len(source):
-	    name = ''
-	    for ind in range(len(source)):
-	        if source[ind] == sep:
-	            i = ind
-	        else:
-	            name += source[ind]
-	    lst.append(name)
-	    string = ''
-	    for index in range(i + 1, len(source)):
-	        string += source[index]
-	    source = string
-	    j += 1 
-	return lst
-	
-string = input("insert string: ")
-count = input("insert count: ")
-print(f"the list consists of the string is: {split(string, ',')}")
+def split(source, sep):
+        i = 0  
+        lst = []
+        str = ''
+        for i in range(len(source)):
+            if source[i] != sep:
+                str += source[i]
+            else:
+                lst.append(str)
+                str = ''
+        return lst
+    
+sep = input("insert the sep: ") 
+source = input("insert the source: ")
+print(f"the new source is: {split(source, sep)}")
