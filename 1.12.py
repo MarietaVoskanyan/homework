cfc1c1120f80290f2bf6f74bed8c9c3623980869
MarietaVoskanyan <107118791+MarietaVoskanyan@users.noreply.github.com>
2022-06-29 02:45:03 +0400

    Update 1.12.py

--- 1.12.py
+++ 1.12.py
@@ -1,5 +1,17 @@
 def pascal(m, n):
    """ pascal finds the number of Pascal's triangle """
-    if m == n or n == 1:
+    if n in (m, 1):
         return 1 
     return pascal(m-1, n-1) + pascal(m-1, n)
+   
+m = int(input("insert m: ")
+n = int(input("insert n: ")
+print(f"The number is: {pascal(m, n)}")  
+        
+        
+def pascal(m, n):
+    return 1 if n in (1, m) else (pascal(m-1, n-1) + pascal(m-1, n))
+   
+m = int(input("insert m: ")
+n = int(input("insert n: ")
+print(f"The number is: {pascal(m, n)}") 
