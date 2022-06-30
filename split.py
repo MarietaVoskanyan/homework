293aab699043bbdf8fd9466986ab7486eb3cc3be
MarietaVoskanyan <107118791+MarietaVoskanyan@users.noreply.github.com>
2022-06-30 18:11:37 +0400

    Update split.py

--- split.py
+++ split.py
@@ -13,3 +13,16 @@ def split(source, sep):
 sep = input("insert the sep: ") 
 source = input("insert the source: ")
 print(f"the new source is: {split(source, sep)}")
+
+
+
+def split(source, sep):
+        len_sep = len(sep)
+        res = []
+        idx = 0
+        for i in range(0, len(source)):
+                if source[i:i+len_sep] == sep:
+                        res.append(source[idx:i])
+                        idx = i + len_sep
+        return res
+'asdfghedrfghmkjl', 'gh'
