ce764341dd202ea900fb0c8ac4553352b4140b0a
MarietaVoskanyan <107118791+MarietaVoskanyan@users.noreply.github.com>
2022-07-05 16:01:05 +0400

    Create prog2.py

---
+++ prog2.py
@@ -0,0 +1,10 @@
+def func(tpl):
+    m = len(tpl) - 1 
+    number = 0
+    for num in tpl:
+        number += (num * (10 ** m))
+        m -= 1 
+    return number
+
+tpl1 = input("insert the number: ")
+print(f'number is: {func(tpl1)}')
